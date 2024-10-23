84328da00da819253c8550f066371484bbcc2d07
Lydia-ma-sketch <kk.lydia@gmail.com>
2024-10-23 10:20:02 +0630

    Integrated AI API in javascript

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: `What is this life, if full of care <br />we have no time, to stand and
-          stare`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let topicElement = document.querySelector("#topic-input");
+  apiKey = "ec3f3cc5ba623cbcd7873aab4b1c3t0o";
+  prompt = `User instructions: Please generate a poem about ${topicElement.value}`;
+  context =
+    "You are a romantic poem expert and you love to write short and sweet poems. Please follow user instructins to write a maximum 4-line poem in basic HTML by separating each line by <br/> excluding html or ```. Please sign at the bottom of the poem as 'SheCodes AI' with a <strong> element. Thank you";
+  apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
+  console.log("generating");
+  console.log(prompt);
+  console.log(context);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
