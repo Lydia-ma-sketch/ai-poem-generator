0911d5a0d8a85ec1557bf19ba647c53048945f31
Lydia-ma-sketch <kk.lydia@gmail.com>
2024-10-23 09:35:14 +0630

    Added basic form handling and typewriter plugin in javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//alert("Hello");
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: `What is this life, if full of care <br />we have no time, to stand and
+          stare`,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
