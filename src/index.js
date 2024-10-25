ee554a8909779026d2b2b0c9c88ca38741d57ae6
Lydia-ma-sketch <kk.lydia@gmail.com>
2024-10-25 09:21:55 +0630

    Modified the project for better user experience

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 2,
     cursor: "",
   });
 }
@@ -16,10 +16,10 @@ function generatePoem(event) {
     "You are a romantic poem expert and you love to write short and sweet poems. Please follow user instructins to write a maximum 4-line poem in basic HTML by separating each line by <br/> excluding html or ```. Please sign at the bottom of the poem as 'SheCodes AI' with a <strong> element. Thank you";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
-  console.log("generating");
-  console.log(prompt);
-  console.log(context);
+  poemElement.classList.add("poem");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${topicElement.value}</div>`;
 }
-
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("poem");
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
